dcfc5df192989b9f625a1fdcdcde9c04d0948455
murilomontino <murilomontinojr@hotmail.com>
2019-12-09 20:58:36 -0200

    version 0.0

---
+++ Projects Anaconda/Sort's/quick_sort.py
@@ -0,0 +1,40 @@
+from random import randint
+import time
+
+def create_list(lista : list):
+    for aux in range(10):
+        lista.append(randint(0,100000))
+    return lista
+
+def quick_sort(lista: list):
+    
+    if len(lista) <= 1:
+        return lista
+    
+    esq : list = []
+    dir_ : list = []
+    pivo : int = lista[randint(0, len(lista)-1 )]
+    
+    for aux in lista:
+        if aux < pivo:
+            esq.append(aux)
+        else:
+            dir_.append(aux)
+
+    esq = quick_sort(esq)
+    
+    dir_ = quick_sort(dir_)
+
+    return esq + dir_
+
+def main():
+    queijo : list = []
+    create_list(queijo)
+    queijo = quick_sort(queijo)
+    print("Ordenado: ", queijo)
+
+if __name__ == "__main__":
+    main()
+
+
+
